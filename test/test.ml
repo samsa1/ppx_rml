2fed937cff3ab2e7db389d7cd2a92558e4b8e4e9
Samuel VIVIEN <samuel.aime.vivien@free.fr>
2022-01-04 15:09:09 +0100

    Added new test for rmlc

--- test/test.ml
+++ test/test.ml
@@ -5,6 +5,7 @@ let () =
       ("Depth search", Depth_search.test_set);
       ("Default", Default.test_set);
       ("Await_when", Await.test_set);
+      ("Await_rml", Await2test.test_set);
       ("Pre", Pre.test_set);
       ("Last", Last.test_set)
     ]
