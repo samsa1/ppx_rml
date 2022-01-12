3721404d2b19b60caaf2e54b15eb756f1f7e7ec3
samsa1 <65660384+samsa1@users.noreply.github.com>
2022-01-12 18:09:33 +0100

    Update sig_env.ml

--- interpreter/sig_env.ml
+++ interpreter/sig_env.ml
@@ -175,7 +175,7 @@ module Class : S =
 
 
         (* maj de l'etat de l'objet si necessaire et revoie "true"
-	    s'il doit etre mis a jour � l'instant suivant *)
+	    s'il doit etre mis a jour a l'instant suivant *)
 	method update =
 	  if to_update then
 	    begin
